0fb26c74d76e701432bf52fec7759c59b0195e9e
abuhureraarain <91952825+abuhureraarain@users.noreply.github.com>
2024-03-14 16:14:07 +0500

    Add files via upload

---
+++ day-001/day001.ts
@@ -0,0 +1,17 @@
+//  Day 1 Challenge: Start Coding! 
+
+
+
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let myName: string = "Abu Hurera";
+console.log(`Hello ${myName} would you like to learn some TypeScript today?`);
+
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let fatherName: string = "Rafaqat Ali";
+console.log(fatherName.toUpperCase());
+console.log(fatherName.toLocaleLowerCase());
+console.log(fatherName.charAt(0).toUpperCase() + fatherName.slice(1).toLocaleLowerCase());
---
+++ day-004/day004.js
@@ -0,0 +1,11 @@
+"use strict";
+// Day 4 Challenge: Start Coding!
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+let myName = "Abu Hurera";
+console.log(`Hello ${myName} would you like to learn some TypeScript today?`);
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+let bestNmaes = ["Muhammad", "Ali", "Abu Hurera"];
+for (let i = 0; i < bestNmaes.length; i++) {
+    const element = bestNmaes[i];
+    console.log(bestNmaes[i]);
+}
